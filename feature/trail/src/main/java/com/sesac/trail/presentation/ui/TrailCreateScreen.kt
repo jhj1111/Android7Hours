33410bd776c642b254f01450ed54f5feabac1539
jhj1111 <happyijun@naver.com>
2025-12-15 15:32:24 +0900

    fix(trail): 산책로 생성 후 뒤로가기 변경

--- feature/trail/src/main/java/com/sesac/trail/presentation/ui/TrailCreateScreen.kt
+++ feature/trail/src/main/java/com/sesac/trail/presentation/ui/TrailCreateScreen.kt
@@ -150,12 +150,13 @@ fun TrailCreateScreen(
                 Log.d("TAG-TrailCreateScreen", "created path : $createdPath")
                 // 수정 화면 스택에서 제거하고, 수정된 상세 화면으로 이동
                 viewModel.resetCreateState()
-                navController.navigate(NestedNavigationRoute.TrailDetail(createdPath.toPathParceler())) {
-                    popUpTo(navController.currentDestination!!.id) {
-                        inclusive = true
-                    }
-                    launchSingleTop = true
-                }
+//                navController.navigate(NestedNavigationRoute.TrailDetail(createdPath.toPathParceler())) {
+//                    popUpTo(navController.currentDestination!!.id) {
+//                        inclusive = true
+//                    }
+//                    launchSingleTop = true
+//                }
+                navController.popBackStack()
 
             }
             is ResponseUiState.Error -> {
